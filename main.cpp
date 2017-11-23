ea589d38633c5dab2e488d9fbc6fdaa213d02d2a
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-11-23 10:18:50 -0500

    Add for loop to print integers 0-4

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ int main(int argc, char const *argv[])
 {
     std::cout << "Hello, world" << std::endl;
 
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << i << "\n";
+    }
+
     return 0;
 }
