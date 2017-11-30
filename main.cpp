d5739c82ec7108f3a5c8144383d6f09750da9746
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-11-30 15:58:46 -0500

    Add Hello there

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello, World" << std::endl;
+    std::cout << "Hello there, world" << std::endl;
 
     for (int i = 0; i < 5; ++i)
     {
